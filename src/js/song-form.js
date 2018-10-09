90d4cdd5815de42d6044c14bad10017fc3f0ab78
yojinlll <yojin1995@qq.com>
2018-10-09 15:25:04 +0800

    添加歌曲新建功能

--- src/js/song-form.js
+++ src/js/song-form.js
@@ -1,6 +1,9 @@
 {
     let view = {
         el: '.page > main',
+        init() {
+            this.$el = $(this.el)
+        },
         template: `
         <h1>新建歌曲</h1>
         <form class="form">
@@ -8,19 +11,19 @@
                 <label>
                     歌名
                 </label>
-                <input type="text" value="__key__">
+                <input name="name" type="text" value="__name__">
             </div>
             <div class="row">
                 <label>
                     歌手
                 </label>
-                <input type="text" value="__singer__">
+                <input name="singer" type="text" value="__singer__">
             </div>
             <div class="row">
                 <label>
                     外链
                 </label>
-                <input type="text" value="__link__">
+                <input name="url" type="text" value="__url__">
             </div>
             <div class="row actions">
                 <button type="submit">保存</button>
@@ -28,25 +31,65 @@
         </form>
         `,
         render(data = {}) {
-            let placeholders = ['key','link','singer']
-            console.log(11111111111)
-            console.log(data)
+            let placeholders = ['name', 'url', 'singer', 'id']
             let html = this.template
             placeholders.map((string) => {
                 html = html.replace(`__${string}__`, data[string] || '')
             })
             $(this.el).html(html)
         },
+        reset(){
+            this.render({})
+        }
     }
-    let model = {}
+    let model = {
+        data: {
+            name: '', singer: '', url: '', id: ''
+        },
+        create(data) {              // 将提交的数据传入 leancould
+            var Song = AV.Object.extend('Song');
+            var song = new Song();
+            song.set('name', data.name);
+            song.set('singer', data.singer);
+            song.set('url', data.url);
+            return song.save().then((newSong) => {
+                let { id, attributes } = newSong
+                Object.assign(this.data, { id, ...attributes })
+            }, (error) => {
+                console.error(error);
+            });
+        }
+    }
+
     let controller = {
         init(view, model) {
             this.view = view
+            this.view.init()
             this.model = model
             this.view.render(this.model.data)
+            this.bindEvents()
             window.eventHub.on('upload', (data) => {
-                this.view.render(data)
+                this.model.data = data
+                this.view.render(this.model.data)
             })
+        },
+        bindEvents() {       // 获取提交的字符串 name singer url
+            this.view.$el.on('submit', 'form', (e) => {
+                e.preventDefault()
+                let needs = 'name singer url'.split(' ')
+                let data = {}
+                needs.map((string) => {
+                    data[string] =this.view.$el.find(`[name=${string}]`).val()
+                })
+                this.model.create(data)
+                    .then(()=>{
+                        this.view.reset()       // 数据传入 leancould 后重置输入框
+                        let string = JSON.stringify(this.model.data)        // 避免旧内存地址导致 bug ，用深拷贝赋予新内存地址
+                        let object = JSON.parse(string)
+                        window.eventHub.emit('create',object)
+                    })
+            })
+
         }
     }
     controller.init(view, model)
--- src/js/upload-song.js
+++ src/js/upload-song.js
@@ -44,8 +44,8 @@
                         var response = JSON.parse(info.response);
                         var sourceLink = domain + '/' + encodeURIComponent(response.key);
                         window.eventHub.emit('upload', {
-                            link: sourceLink,
-                            key: response.key
+                            url: sourceLink,
+                            name: response.key
                         })
                     },
                     'Error': function (up, err, errTip) {
