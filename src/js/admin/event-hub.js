c182e9e85688b620ad7a7ff90158de7cc7faf1be
yojinlll <yojin1995@qq.com>
2018-10-13 22:05:06 +0800

    转盘动画同步暂停播放

--- src/js/admin/event-hub.js
+++ src/js/admin/event-hub.js
@@ -3,7 +3,7 @@ window.eventHub = {
         //'羊城晚报': [fn],
         //'楚天都市报': [],
     }, // hash
-    emit(eventName, data) { //发布
+    emit(eventName, data) {         // 发布，根据 eventName 去执行相应的 fn，并给 fn 传 data 参数
         for (let key in this.events) {
             if (key === eventName) {
                 let fnList = this.events[key]
@@ -13,7 +13,7 @@ window.eventHub = {
             }
         }
     },
-    on(eventName, fn) { //订阅
+    on(eventName, fn) {             // 订阅，把 eventName 和 fn 两两对应 push 到 events
         if (this.events[eventName] === undefined) {
             this.events[eventName] = []
         }
--- src/js/song/app.js
+++ src/js/song/app.js
@@ -1,26 +1,31 @@
 {
   let view = {
     el: '#app',
-    render(data){
-      let {song,status} = data
-      $(this.el).css('background-image',`url(${song.cover})`)   // 页面背景
-      $(this.el).find('img.cover').attr('src', song.cover)      // 转盘背景 
+    init() {
+      this.$el = $(this.el)
+    },
+    render(data) {
+      let { song, status } = data
+      this.$el.css('background-image', `url(${song.cover})`)                  // 页面背景
+      this.$el.find('img.cover').attr('src', song.cover)                      // 转盘背景 
 
-      if($(this.el).find('audio').attr('src') !== song.url){    // 若 url 相等就不渲染，让歌曲连贯
-        $(this.el).find('audio').attr('src', song.url)
+      if (this.$el.find('audio').attr('src') !== song.url) {                  // 若 url 相等就不渲染，让歌曲连贯
+        this.$el.find('audio').attr('src', song.url)
       }
-
-      if(status==='playing'){
-        $(this.el).find('.disc-container').addClass('playing')
-      }else{
-        $(this.el).find('.disc-container').removeClass('playing')
+      let audio = this.$el.find('audio')[0]
+      audio.onended = () => { window.eventHub.emit('songEnd') }               // 调用 audio.onended 执行 songEnd 事件，去暂停转盘
+      
+      if (status === 'playing') {
+        this.$el.find('.disc-container').addClass('playing')
+      } else {
+        this.$el.find('.disc-container').removeClass('playing')
       }
     },
-    play(){
-      $(this.el).find('audio')[0].play()
+    play() {
+      this.$el.find('audio')[0].play()
     },
-    pause(){
-      $(this.el).find('audio')[0].pause()
+    pause() {
+      this.$el.find('audio')[0].pause()
     }
   }
   let model = {
@@ -31,14 +36,13 @@
         singer: '',
         url: '',
       },
-      status:'paused'
+      status: 'playing'
 
     },
     get(id) {
       var query = new AV.Query('Song')
-      return query.get(id).then((song) => {
-        console.log(song)
-        Object.assign(this.data.song, { id: song.id, ...song.attributes })    // setId 获取了id，get 获取其他数据
+      return query.get(id).then((song) => {             // 使用 leancould API 根据 id 获取其他数据，url
+        Object.assign(this.data.song, { id: song.id, ...song.attributes })
         return song
       })
     }
@@ -46,6 +50,7 @@
   let controller = {
     init(view, model) {
       this.view = view
+      this.view.init()
       this.model = model
       let id = this.getSongId()
       this.model.get(id).then(() => {
@@ -56,23 +61,27 @@
 
     },
     bindEvents() {
-      $(this.view.el).on('click','.icon-play',()=>{
-        this.model.data.status = 'playing'
+      $(this.view.el).on('click', '.icon-pause', () => {
+        this.model.data.status = 'paused'
         this.view.render(this.model.data)
         this.view.pause()
       })
-      $(this.view.el).on('click','.icon-pause',()=>{
-        this.model.data.status = 'paused'
+      $(this.view.el).on('click', '.icon-play', () => {
+        this.model.data.status = 'playing'
         this.view.render(this.model.data)
         this.view.play()
+      })
+      window.eventHub.on('songEnd', () => {
+        this.model.data.status = 'paused'
+        this.view.render(this.model.data)
       })
     },
     getSongId() {
       let search = window.location.search
       if (search.indexOf('?') === 0) {
-        search = search.substring(1)
+        search = search.substring(1)                    // 获取 ? 之后的字符串（参数的合集）
       }
-      let array = search.split('&').filter(v => v)      // 过滤空字符串 (v)=>v
+      let array = search.split('&').filter(v => v)      // 以'&'为基准分节，并过滤空字符串 (v)=>v
       let id = ''
 
       for (let i = 0; i < array.length; i++) {
